e426b13fb0d643741d08312783e75293317b8938
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2026-01-06 21:45:27 +0900

    Implement a REPL step complete

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,6 +1,8 @@
 fun main() {
     // TODO: Uncomment the code below to pass the first stage
-     print("$ ")
-     val command = readlnOrNull() ?: return
-     println("$command: command not found")
+    while (true) {
+        print("$ ")
+        val command = readlnOrNull() ?: return
+        println("$command: command not found")
+    }
 }
