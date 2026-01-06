11bfe06a90479efff49f681b903eb5bddde557b7
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2026-01-06 22:16:24 +0900

    Implement type step complete

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -2,25 +2,38 @@ fun main() {
     // TODO: Uncomment the code below to pass the first stage
     while (true) {
         print("$ ")
-        val command = readlnOrNull() ?: return
-        if (command.startsWith("exit")) break
+        val line = readlnOrNull() ?: return
+        if (line.startsWith("exit")) break
 
-        val commands = parseCommand(command)
-        executeCommand(commands)
+        val commands = parseCommand(line)
+        when (val command = ShellCommand.from(commands[0])) {
+            ShellCommand.EXIT -> return
+            ShellCommand.ECHO -> println(commands.subList(1, commands.size).joinToString(" "))
+            ShellCommand.TYPE -> {
+                val nextCommand = ShellCommand.from(commands[1])
+                if (nextCommand == ShellCommand.NONE) println(
+                    "${commands.subList(1, commands.size).joinToString(" ")}: not found"
+                )
+                else println("${nextCommand.value} is a shell builtin")
+            }
+
+            ShellCommand.NONE -> println("$line: command not found")
+        }
     }
 }
 
 private fun parseCommand(line: String): List<String> = line.split(" ")
-private fun executeCommand(commands: List<String>) {
-    val command = commands[0];
-    when (command) {
-        "echo" -> {
-            println(
-                commands.subList(1, commands.size)
-                    .joinToString(" ")
-            )
-        }
 
-        else -> println("$command: command not found")
+enum class ShellCommand(val value: String) {
+    ECHO("echo"),
+    EXIT("exit"),
+    TYPE("type"),
+    NONE("none");
+
+    companion object {
+        fun contains(value: String): Boolean =
+            entries.any { entry -> entry.value == value }
+
+        fun from(value: String): ShellCommand = entries.firstOrNull { it.value == value } ?: NONE
     }
 }
