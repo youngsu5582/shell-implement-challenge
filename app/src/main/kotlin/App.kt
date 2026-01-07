a9b611907291953b85e26af74985e75300f5a920
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2026-01-07 23:42:55 +0900

    Locate executable files step complete

--- app/src/main/kotlin/App.kt
+++ app/src/main/kotlin/App.kt
@@ -1,3 +1,21 @@
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.NoSuchFileException
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.Path
+import kotlin.io.path.absolute
+import kotlin.io.path.absolutePathString
+import kotlin.io.path.isDirectory
+import kotlin.io.path.isExecutable
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.name
+import kotlin.io.path.pathString
+
+object Constant {
+    const val PATH = "PATH"
+}
+
 fun main() {
     // TODO: Uncomment the code below to pass the first stage
     while (true) {
@@ -10,25 +28,61 @@ fun main() {
             ShellCommand.EXIT -> return
             ShellCommand.ECHO -> println(commands.subList(1, commands.size).joinToString(" "))
             ShellCommand.TYPE -> {
-                val nextCommand = ShellCommand.from(commands[1])
-                if (nextCommand == ShellCommand.NONE) println(
-                    "${commands.subList(1, commands.size).joinToString(" ")}: not found"
-                )
-                else println("${nextCommand.value} is a shell builtin")
+                // type ls
+                // ls
+                val nextCommand = commands[1]
+                if (ShellCommand.from(nextCommand).type == ShellCommandType.BUILT_IN) {
+                    println("$nextCommand is a shell builtin")
+                    continue
+                }
+                val result = findExecutable(nextCommand)
+                if (result != null) {
+                    println("$nextCommand is ${result.pathString}")
+                    continue
+                } else {
+                    println("${commands.subList(1, commands.size).joinToString(" ")}: not found")
+                }
             }
 
-            ShellCommand.NONE -> println("$line: command not found")
+            else -> println("$line: command not found")
         }
     }
 }
 
+private fun findExecutable(command: String): Path? {
+    val pathList = System.getenv(Constant.PATH).split(":")
+    for (path in pathList) {
+        val path = Path(path)
+        val found = recursiveSearch(path, command)
+        if (found != null) return found
+    }
+    return null
+}
+
+private fun recursiveSearch(path: Path, to: String): Path? {
+    try {
+        val entries = path.listDirectoryEntries()
+        for (entry in entries) {
+            if (entry.isDirectory()) {
+                val recursiveFound = recursiveSearch(entry, to)
+                if (recursiveFound != null) return recursiveFound
+            }
+            if (entry.name == to && entry.isExecutable()) return entry.toAbsolutePath()
+        }
+    } catch (e: Exception) {
+        return null
+    }
+    return null
+}
+
 private fun parseCommand(line: String): List<String> = line.split(" ")
 
-enum class ShellCommand(val value: String) {
-    ECHO("echo"),
-    EXIT("exit"),
-    TYPE("type"),
-    NONE("none");
+enum class ShellCommand(val value: String, val type: ShellCommandType) {
+    ECHO("echo", ShellCommandType.BUILT_IN),
+    EXIT("exit", ShellCommandType.BUILT_IN),
+    TYPE("type", ShellCommandType.BUILT_IN),
+    LS("ls", ShellCommandType.EXECUTABLE),
+    NONE("none", ShellCommandType.EXECUTABLE);
 
     companion object {
         fun contains(value: String): Boolean =
@@ -36,4 +90,9 @@ enum class ShellCommand(val value: String) {
 
         fun from(value: String): ShellCommand = entries.firstOrNull { it.value == value } ?: NONE
     }
+}
+
+enum class ShellCommandType {
+    BUILT_IN,
+    EXECUTABLE
 }
